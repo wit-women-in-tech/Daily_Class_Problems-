797d71d0d5573bea7a4f4b774694145c05497ffc
Anshika Jain <anshikajain@Anshikas-MacBook-Air.local>
2022-11-11 21:43:47 +0530

    day 1

---
+++ test.cpp
@@ -0,0 +1,13 @@
+//Print your age by creating a variable 
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // char x='anshu';
+    // cout<<x<<endl;
+    // char y=false;
+    // cout<<y<<endl;
+    cout<<sizeof(long)<<" "<<sizeof(int)<<endl;
+}
