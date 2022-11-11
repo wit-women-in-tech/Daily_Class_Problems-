b9c05272b092fc61b2257850daf5afe442afb720
Anshika Jain <anshikajain@Anshikas-MacBook-Air.local>
2022-11-11 21:11:58 +0530

    first commit

---
+++ Day1/age.cpp
@@ -0,0 +1,10 @@
+//Print your age by creating a variable 
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int age=20;
+    cout<<age<<endl;
+}
---
+++ Day1/name.cpp
@@ -0,0 +1,9 @@
+//Print your name using cout statement
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Women in tech ++";
+}
